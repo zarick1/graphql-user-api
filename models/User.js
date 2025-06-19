6f4e4c7d0eba4d59e891ce0614fc520ec7ab0394
zarick1 <krstozaric01@gmail.com>
2025-06-19 18:31:49 +0200

    Added try/catch blocks and email validation

--- models/User.js
+++ models/User.js
@@ -15,4 +15,5 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model('User', userSchema);
+
 export default User;
--- schema/resolvers.js
+++ schema/resolvers.js
@@ -3,15 +3,29 @@ import User from '../models/User.js';
 const resolvers = {
   Query: {
     users: async () => {
-      return await User.find();
+      try {
+        return await User.find();
+      } catch (error) {
+        throw new Error('Error fetching users');
+      }
     },
   },
 
   Mutation: {
     addUser: async (_, { name, email }) => {
-      const user = new User({ name, email });
-      await user.save();
-      return user;
+      try {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+          throw new Error('Invalid email format');
+        }
+
+        const user = new User({ name, email });
+        await user.save();
+        return user;
+      } catch (error) {
+        throw new Error('Error adding user:' + error.message);
+      }
     },
   },
 };
